f5186f8201bbbc39ddf89c792ff474e35e5f7e7a
Christian Storch <ccstorch@gmail.com>
2025-08-10 20:42:55 +0200

    Test react/native component

--- src/SvgTest.js
+++ src/SvgTest.js
@@ -5,7 +5,7 @@ export function SvgTest({ size = 100 }) {
   return (
     <View>
       <Svg height={size} width={size}>
-        <Circle cx={size / 2} cy={size / 2} r={size / 3} fill="orange" />
+        <Circle cx={size / 2} cy={size / 2} r={size / 3} fill="green" />
       </Svg>
     </View>
   )
