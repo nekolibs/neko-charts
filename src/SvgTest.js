26572463676ed17fff8d9787e9688f2b7a331095
Christian Storch <ccstorch@gmail.com>
2025-08-10 20:36:31 +0200

    Init

---
+++ src/SvgTest.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import Svg, { Circle } from 'react-native-svg'
+
+export function SvgTest({ size = 100 }) {
+  return (
+    <View>
+      <Svg height={size} width={size}>
+        <Circle cx={size / 2} cy={size / 2} r={size / 3} fill="orange" />
+      </Svg>
+    </View>
+  )
+}
